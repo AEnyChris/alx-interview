378ebfcb59f24fd448f73fbef00b62e4ee37f381
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-16 23:40:19 +0100

    refactored

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-""" Module for 0-minoperations"""
+"""The minimum operations challenge"""
 
 
 def minOperations(n):
     """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    Computes the minimum operations to get n H
     """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
     if (n < 2):
         return 0
-    ops, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return ops
+    ops_sum, divisor = 0, 2
+    while divisor <= n:
+        if n % divisor == 0:
+            ops_sum += divisor
+            n = n / divisor
+        else:
+            divisor += 1
+    return ops_sum
