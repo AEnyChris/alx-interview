5e4ed147d150f155def39e05cf4c00649c2f9bc7
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-03 03:30:21 +0100

    utf-8 validat v2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-"""A utf8 validation script"""
+"""validate utf8"""
+
 
 def validUTF8(data):
     """
-    A function that validates a data set to be UTF8 comliant
-    Returns True if valid else False
+    checks if a set of data is utf8 valid
+    Return: boolean
     """
-    number_bytes = 0
+    mask = 1 << 7
 
-    mask_1 = 1 << 7
-    # print(f'mask_1: {mask_1}, bin of mask {bin(mask_1)}')
-    mask_2 = 1 << 6
-    # print(f'mask_2: {mask_2}, bin of mask {bin(mask_2)}')
+    for byte in data:
+        if byte < 0x7f:
+            continue
 
-    for i in data:
+        leading_bits = 0
 
-        mask_byte = 1 << 7
-        # print(f'mask_byte: {mask_byte}, bin of mask {bin(mask_byte)}')
+        while mask & byte:
+            leading_bits += 1
+            mask = mask >> 1
 
-        if number_bytes == 0:
-           # print(f'number_bytes: {number_bytes}')
+        if leading_bits == 1 or leading_bits > 4:
+            return False
 
-            while mask_byte & i:
-               # print(f'mask_byte & i: {mask_byte & i}')
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-               # print(f'mask_byte end of while loop: {mask_byte}')
-               # print(f'number_byte: {number_bytes}')
+        lsb = byte & mask
 
-            if number_bytes == 0:
-                continue
-
-            if number_bytes == 1 or number_bytes > 4:
-                return False
-
-        else:
-            if not (i & mask_1 and not (i & mask_2)):
-                    return False
-
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False
+        if not(lsb & mask and not (lsb & (mask >> 1))):
+            return False
+    return True
