14964767a5a24cd15e3a3c5e134a6acf8c89eea5
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-03 22:24:37 +0100

    correction: modify to check continuation bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,22 +8,31 @@ def validUTF8(data):
     Return: boolean
     """
     mask = 1 << 7
-
+    leading_bits = 0
     for byte in data:
+        # print(f'checking byte: {byte}')
+        byte = byte & 0xFF
+        mask_byte = mask
+
         if byte < 0x7f:
             continue
+        # check leading bits of utf8 bytes
+        if leading_bits == 0:
+            while mask_byte & byte:
+                leading_bits += 1
+                # print(f'\tleading_bits: {leading_bits}')
+                mask_byte = mask_byte >> 1
 
-        leading_bits = 0
+            if leading_bits == 1 or leading_bits > 4:
+                # print('\tchecking leading bits')
+                return False
 
-        while mask & byte:
-            leading_bits += 1
-            mask = mask >> 1
+        else:
+            if not (byte & mask and not (byte & (mask >> 1))):
+                # print('\tchecking for continuation byte')
+                return False
 
-        if leading_bits == 1 or leading_bits > 4:
-            return False
-
-        lsb = byte & mask
-
-        if not(lsb & mask and not (lsb & (mask >> 1))):
-            return False
+        leading_bits -= 1
+    if leading_bits != 0:
+        return False
     return True
