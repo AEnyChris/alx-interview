76d755b6eea49b407e755a17b18a14a4f81a9b27
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-12-06 09:49:30 +0100

    change variable name

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,16 +4,16 @@
 
 def island_perimeter(grid) -> int:
     """returns the perimeter of island described in grid"""
-    sum_land = 0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 if (i == 0 or grid[i - 1][j] == 0):
-                    sum_land += 1
+                    perimeter += 1
                 if (i == len(grid) - 1 or grid[i + 1][j] == 0):
-                    sum_land += 1
+                    perimeter += 1
                 if (j == 0 or grid[i][j - 1] == 0):
-                    sum_land += 1
+                    perimeter += 1
                 if (j == len(grid[0]) - 1 or grid[i][j + 1] == 0):
-                    sum_land += 1
-    return sum_land
+                    perimeter += 1
+    return perimeter
